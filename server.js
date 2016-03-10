881cb9579a4972aed2ead2374638d659ee062e0d
Vova Paskiv <vovaaskiv@gmail.com>
2016-03-10 17:14:46 +0200

    port

--- server.js
+++ server.js
@@ -15,5 +15,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/*', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
-app.listen(3000);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 console.log("Go Prerender Go!");
