80f1ecabf6a549c37a3e92de60c7e6dff8cadce6
Vova Paskiv <vovaaskiv@gmail.com>
2016-03-10 17:51:19 +0200

    Procfile

--- server.js
+++ server.js
@@ -2,13 +2,10 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var morgan = require('morgan');
-
 
 // Here we require the prerender middleware that will handle requests from Search Engine crawlers
 // We set the token only if we're using the Prerender.io service
 app.use(require('prerender-node').set('prerenderToken', 'YOUR-TOKEN-HERE'));
-app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
 
